afa086637f77d844aefb7faa0a7b02135af37d78
Thomas Manfredo <112675181+tmanfredo@users.noreply.github.com>
2024-10-01 19:13:02 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+
+	etag "github.com/pablor21/echo-etag/v4"
+
+	"go-store/templates"
+)
+
+
+func main() {
+	e := echo.New()
+	e.Use(etag.Etag())
+	// INFO: If you wanted to load a CSS file, you'd do something like this:
+	// `<link rel="stylesheet" href="assets/styles/styles.css">`
+	e.Static("assets", "./assets")
+	e.GET("/store", func(ctx echo.Context) error {
+		return Render(ctx, http.StatusOK, templates.Base())
+	})
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+// INFO: This is a simplified render method that replaces `echo`'s with a custom
+// one. This should simplify rendering out of an echo route.
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	buf := templ.GetBuffer()
+	defer templ.ReleaseBuffer(buf)
+
+	if err := t.Render(ctx.Request().Context(), buf); err != nil {
+		return err
+	}
+
+	return ctx.HTML(statusCode, buf.String())
+}
