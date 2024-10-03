5c8106417541236a5255e6786bb046fadf4543f3
Thomas Manfredo <tmanfredo@mines.edu>
2024-10-02 18:43:14 -0600

    fixed skeleton code for initial needs

--- go.mod
+++ go.mod
@@ -2,16 +2,18 @@ module go-store
 
 go 1.22.7
 
-require github.com/labstack/echo v3.3.10+incompatible
+require (
+	github.com/a-h/templ v0.2.778
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/pablor21/echo-etag/v4 v4.0.3
+)
 
 require (
-	github.com/a-h/templ v0.2.778 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/pablor21/echo-etag/v4 v4.0.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 	etag "github.com/pablor21/echo-etag/v4"
 
 	"go-store/templates"
-	"go-store/types"
+	//"go-store/types"
 )
 
 func main() {
 	// TODO: Fill in your products here with name -> price as the key -> value pair.
-	products := map[string]float64{
-	}
+	// products := map[string]float64{
+	// }
 	e := echo.New()
 	e.Use(etag.Etag())
 
@@ -25,17 +25,17 @@ func main() {
 
 	// TODO: Render your base store page here
 	e.GET("/store", func(ctx echo.Context) error {
-		return Render(ctx, http.StatusOK, templates.Base(templates.Store(...)))
+		return Render(ctx, http.StatusOK, templates.Base())
 	})
 
 	// TODO: Handle the form submission and return the purchase confirmation view
 	e.POST("/purchase", func(ctx echo.Context) error {
 		// TODO: Grab the form details from ctx.FormValue("...")
-		purchaseInfo := types.PurchaseInfo{
-			// TODO: Maybe use this structure to pass the data to your purchase confirmation page
-			// ...
-		}
-		return Render(ctx, http.StatusOK, templates.Base(templates.PurchaseConfirmation(purchaseInfo)))
+		// purchaseInfo := types.PurchaseInfo{
+		// 	// TODO: Maybe use this structure to pass the data to your purchase confirmation page
+		// 	// ...
+		// }
+		return Render(ctx, http.StatusOK, templates.Base())
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
